ee0b549b171c2dad726da9036ff2285086adc15d
Christian Moniz <xianmoniz@gmail.com>
2014-08-10 18:28:11 -0700

    replaced tabs with spaces

--- hangman.rb
+++ hangman.rb
@@ -1,103 +1,114 @@
 class Hangman
-	MAX_ATTEMPTS = 10
+  MAX_ATTEMPTS = 10
 
-	def initialize(guesser, checker, max_attempts = MAX_ATTEMPTS)
-		@guesser = guesser
-		@checker = checker
-		@max_attempts = max_attempts
-	end
+  def initialize(guesser, checker, max_attempts = MAX_ATTEMPTS)
+    @guesser = guesser
+    @checker = checker
+    @max_attempts = max_attempts
+  end
 
-	def play
-		secret_word_length = @checker.pick_secret_word
-		@guesser.receive_secret_word(secret_word_length)
-		@secret_word = Array.new(secret_word_length)
-		
-		@attempts = 0
+  def play
+    secret_word_length = @checker.pick_secret_word
+    @guesser.receive_secret_word(secret_word_length)
+    @secret_word = Array.new(secret_word_length)
+    
+    @attempts = 0
 
-		until game_over?
-			puts "Secret word: #{secret_word}"
+    until game_over?
+      puts "Secret word: #{secret_word}"
 
-			begin
-				print "> "
-				guess = @guesser.guess
-			end until self.class.valid_guess?(guess)
+      begin
+        print "> "
+        guess = @guesser.guess
+      end until self.class.valid_guess?(guess)
 
-			positions = @checker.check_guess(guess)
+      positions = @checker.check_guess(guess)
 
-			@attempts += 1 if positions.empty?
-		end
-	end
+      unless positions.empty?
+        positions.each do |i|
+          @secret_word[i] = guess
+        end
+      else
+        @attempts += 1
+        puts "Incorrect, you have #{@max_attempts - @attempts} attempts left!"
+      end
+    end
+  end
 
-	def game_over?
-		@attempts >= @max_attempts
-	end
+  def game_over?
+    @attempts >= @max_attempts && @secret_word.none? { |el| el.nil? }
+  end
 
-	def secret_word
-		@secret_word.map { |char| char.nil? ? "_" : char }.join(" ")
-	end
+  def secret_word
+    @secret_word.map { |char| char.nil? ? "_" : char }.join(" ")
+  end
 
-	def self.valid_guess?(guess)
-		guess.strip.length > 0 &&
-		guess.downcase.strip.each_char.all? { |char| char.between?('a', 'z') }
-	end
+  def self.valid_guess?(guess)
+    guess.strip.length > 0 &&
+    guess.downcase.strip.each_char.all? { |char| char.between?('a', 'z') }
+  end
 end
 
 class ComputerPlayer
-	DICTIONARY_FILE = "dictionary.txt"
+  DICTIONARY_FILE = "dictionary.txt"
 
-	def initialize(file_name = DICTIONARY_FILE)
-		@dictionary = File.readlines(file_name).map(&:chomp)
-	end
+  def initialize(file_name = DICTIONARY_FILE)
+    @dictionary = File.readlines(file_name).map(&:chomp)
+  end
 
-	def pick_secret_word
-		@secret_word = @dictionary.sample.split('')
-		@secret_word.length
-	end
+  def pick_secret_word
+    @secret_word = @dictionary.sample.split('')
+    @secret_word.length
+  end
 
-	def receive_secret_word(length)
-		@secret_word = Array.new(length)
-	end
+  def receive_secret_word(length)
+    @secret_word = Array.new(length)
+  end
 
-	def guess
-		('a'..'z').to_a.sample
-	end
+  def guess
+    ('a'..'z').to_a.sample
+  end
 
-	def check_guess(guess)
-		[]
-	end
+  def check_guess(guess)
+    [].tap do |positions|
+      @secret_word.each_with_index { |c, i| positions << i if c == guess }
+    end
+  end
 end
 
 class HumanPlayer
-	def initialize
-	end
+  def initialize
+  end
 
-	def pick_secret_word
-		print "Think of a word. How long is it? "
+  def pick_secret_word
+    print "Think of a word. How long is it? "
 
-		begin
-			Integer(gets.chomp)
-		rescue ArgumentError
-			print "Invalid length, try again: "
-			retry
-		end
-	end
+    begin
+      Integer(gets.chomp)
+    rescue ArgumentError
+      print "Invalid length, try again: "
+      retry
+    end
+  end
 
-	def receive_secret_word(length)
-		puts "The word you have to guess is #{length} characters long."
-	end
+  def receive_secret_word(length)
+    puts "The word you have to guess is #{length} characters long."
+  end
 
-	def guess
-		gets.chomp
-	end
+  def guess
+    gets.chomp
+  end
 
-	def check_guess(guess)
-		[]
-	end
+  def check_guess(guess)
+    print "Where do any '#{guess}'s occur, if any (comma seperated): "
+    
+    positions = gets.chomp
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
-	guesser = HumanPlayer.new
-	checker = ComputerPlayer.new
-	game = Hangman.new(guesser, checker)
-	game.play
+  guesser = HumanPlayer.new
+  checker = ComputerPlayer.new
+  game = Hangman.new(guesser, checker)
+  game.play
 end
